8ad0afdbcc374d4e930ea9a05b68957522cce510
Alexis Landa <landa.gomez@gmail.com>
2017-10-21 13:31:41 -0400

    Cambie algo local.

--- JavaTesting/src/com/mycompany/myproject/HelloWorldMain.java
+++ JavaTesting/src/com/mycompany/myproject/HelloWorldMain.java
@@ -3,7 +3,7 @@ package com.mycompany.myproject;
 public class HelloWorldMain {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World");//le agregue la extension
 
 	}
 
